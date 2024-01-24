ec05695edbce89c1f3ae44b7deb50ea6f191848c
SnailCape <koopacapey@gmail.com>
2024-01-24 13:54:07 +0100

    Account, again FINALLY got a good framework for what Ia ctually want from it, will fix this today or tomorrow.

--- MVVM/Views/EventsAccountCP.xaml.cs
+++ MVVM/Views/EventsAccountCP.xaml.cs
@@ -1,3 +1,5 @@
+
+
 namespace OuroborosEvents.MVVM.Views;
 
 public partial class EventsAccountCP : ContentPage
@@ -6,4 +8,23 @@ public partial class EventsAccountCP : ContentPage
 	{
 		InitializeComponent();
 	}
-}
+
+    private async void ImageButton_Clicked(object sender, EventArgs e)
+    {
+        if (MediaPicker.Default.IsCaptureSupported)
+        {
+            //LOAD PHOTO
+            FileResult photo = await MediaPicker.Default.PickPhotoAsync();
+            if (photo != null)
+            {
+                //SAVE THE IMAGE
+                AccountCPProfilePicture.Source = ImageSource.FromStream(() => photo.OpenReadAsync().Result);
+            }
+        }
+        else
+        {
+            await Shell.Current.DisplayAlert("Oops", "Your device isn't supported", "Ok");
+        }
+        }
+    }
+
